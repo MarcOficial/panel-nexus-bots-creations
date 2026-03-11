cbf443b550e31aeffe60aa869841bd2fdc6ecb1b
MarcOficial <llabresmarc083@gmail.com>
2026-03-11 16:08:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,114 +1,32 @@
-const { jsPDF } = window.jspdf;
+function exportPDF(numero){
 
-let presupuestos = JSON.parse(localStorage.getItem("presupuestos")) || [];
+let p = presupuestos.find(x => x.numero == numero);
 
-function showPage(page){
+let doc = new jsPDF();
 
-document.querySelectorAll(".page").forEach(p=>{
-p.style.display="none";
-});
+let img = new Image();
+img.src = "nexus-bots-creations.png";
 
-document.getElementById(page).style.display="block";
+img.onload = function(){
 
-actualizar();
+doc.addImage(img, "PNG", 20, 10, 50, 20);
 
-}
+doc.setFontSize(18);
+doc.text("Presupuesto #" + p.numero, 20, 40);
 
-showPage("dashboard");
+doc.setFontSize(12);
 
-document.getElementById("form").addEventListener("submit",function(e){
+doc.text("Cliente: " + p.cliente, 20, 60);
+doc.text("Discord: " + p.discord, 20, 70);
+doc.text("Servicio: " + p.servicio, 20, 80);
+doc.text("Descripción: " + p.descripcion, 20, 90);
+doc.text("Precio: €" + p.precio, 20, 100);
 
-e.preventDefault();
+doc.text("Empresa: Nexus Bots Creations", 20, 120);
+doc.text("Método de pago: PayPal / Robux", 20, 130);
 
-let cliente=document.getElementById("cliente").value;
-let discord=document.getElementById("discord").value;
-let servicio=document.getElementById("servicio").value;
-let descripcion=document.getElementById("descripcion").value;
-let precio=document.getElementById("precio").value;
-
-let numero=presupuestos.length+1;
-
-let presupuesto={
-
-numero,
-cliente,
-discord,
-servicio,
-descripcion,
-precio
+doc.save("presupuesto-" + p.numero + ".pdf");
 
 };
 
-presupuestos.push(presupuesto);
-
-localStorage.setItem("presupuestos",JSON.stringify(presupuestos));
-
-this.reset();
-
-alert("Presupuesto creado");
-
-actualizar();
-
-showPage("lista");
-
-});
-
-function actualizar(){
-
-document.getElementById("total").innerText=presupuestos.length;
-
-let cont=document.getElementById("presupuestos");
-
-if(!cont)return;
-
-cont.innerHTML="";
-
-presupuestos.forEach(p=>{
-
-let div=document.createElement("div");
-
-div.className="presupuesto";
-
-div.innerHTML=`
-
-<h3>Presupuesto #${p.numero}</h3>
-
-Cliente: ${p.cliente}<br>
-Servicio: ${p.servicio}<br>
-Precio: €${p.precio}<br><br>
-
-<button onclick="exportPDF(${p.numero})">Exportar PDF</button>
-
-`;
-
-cont.appendChild(div);
-
-});
-
 }
-
-function exportPDF(numero){
-
-let p=presupuestos.find(x=>x.numero==numero);
-
-let doc=new jsPDF();
-
-doc.setFontSize(20);
-doc.text("Nexus Bots Creations",20,20);
-
-doc.setFontSize(14);
-
-doc.text("Presupuesto #"+p.numero,20,40);
-doc.text("Cliente: "+p.cliente,20,50);
-doc.text("Discord: "+p.discord,20,60);
-doc.text("Servicio: "+p.servicio,20,70);
-doc.text("Descripción: "+p.descripcion,20,80);
-doc.text("Precio: €"+p.precio,20,90);
-
-doc.text("Pago: PayPal / Robux",20,110);
-
-doc.save("presupuesto-"+p.numero+".pdf");
-
-}
-
-actualizar();
